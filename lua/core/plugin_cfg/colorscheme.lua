9621f01944baa881ec0cda7d17945f9f23c83c7d
Sébastien Sauzon <sebs2@live.fr>
2024-10-05 17:13:03 +0200

    kana back

--- lua/core/plugin_cfg/colorscheme.lua
+++ lua/core/plugin_cfg/colorscheme.lua
@@ -1,18 +1,13 @@
 vim.o.termguicolors = true
 -- kanagawa
---vim.cmd [[ colorscheme kanagawa ]]
---vim.cmd [[ highlight Normal guibg=#282a36]]
---vim.cmd [[ highlight NormalNC guibg=#1f1f28]]
---vim.cmd [[ highlight NormalFloat guibg=#282a36]]
---vim.cmd [[ highlight TabLineSel guifg=#dcd7ba guibg=#2d4f67 gui=bold]]
--- nightfly
---vim.cmd [[ colorscheme nightfly ]]
---vim.cmd [[ highlight NormalNC guibg=#01111e]]
---vim.cmd [[ highlight NormalFloat guibg=#011627]]
---vim.cmd [[ highlight TabLineSel guifg=#dcd7ba guibg=#2d4f67 gui=bold]]
--- onenord
-vim.cmd [[ colorscheme onenord ]]
-vim.cmd [[ highlight Normal guibg=#2e3440]]
-vim.cmd [[ highlight NormalNC guibg=#262b35]]
-vim.cmd [[ highlight NormalFloat guibg=#2e3440]]
+vim.cmd [[ colorscheme kanagawa ]]
+vim.cmd [[ highlight Normal guibg=#282a36]]
+vim.cmd [[ highlight NormalNC guibg=#1f1f28]]
+vim.cmd [[ highlight NormalFloat guibg=#282a36]]
 vim.cmd [[ highlight TabLineSel guifg=#dcd7ba guibg=#2d4f67 gui=bold]]
+-- onenord
+--vim.cmd [[ colorscheme onenord ]]
+--vim.cmd [[ highlight Normal guibg=#2e3440]]
+--vim.cmd [[ highlight NormalNC guibg=#262b35]]
+--vim.cmd [[ highlight NormalFloat guibg=#2e3440]]
+--vim.cmd [[ highlight TabLineSel guifg=#dcd7ba guibg=#2d4f67 gui=bold]]
