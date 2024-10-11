ddda367d820308a83240d740185a02633f7f3899
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-11 09:50:02 +0200

    feat: change diagnostic view in lsp config file (e.g. diag messages not displayed in the buffer)

--- lua/core/plugin_cfg/lspconfig.lua
+++ lua/core/plugin_cfg/lspconfig.lua
@@ -7,10 +7,18 @@ require("lspconfig").verible.setup {}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.diagnostic.config({
+  virtual_text     = false,
+  signs            = true,
+  underline        = false,
+  update_in_insert = false,
+  severity_sort    = false
+})
+
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>lp', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>ln', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>ll', vim.diagnostic.setloclist)
 
 vim.diagnostic.config({
   virtual_text     = false,
