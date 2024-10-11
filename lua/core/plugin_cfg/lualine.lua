90fd03844c2ae51f292bd76091399b5ff9be065e
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-11 09:48:06 +0200

    remove 'ayu' theme in lualine config

--- lua/core/plugin_cfg/lualine.lua
+++ lua/core/plugin_cfg/lualine.lua
@@ -1,7 +1,6 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    -- theme = 'ayu',
     disabled_filetypes = { 'packer', 'NvimTree' },
     component_separators = '',
     section_separators = '',
