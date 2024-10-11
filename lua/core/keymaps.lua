50b2070e680bbf4f8b22d82870c7e5eef02749fb
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-11 09:45:27 +0200

    refactor: add ascii 'art' key shortcuts as comments in keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,10 +1,34 @@
-vim.keymap.set('n', "<C-Left>", ":bprev <CR>", noremap)
-vim.keymap.set('n', "<C-Right>", ":bnext <CR>", noremap)
-vim.keymap.set('n', "<C-Down>", ":m+<CR>==", noremap)
-vim.keymap.set('n', "<C-Up>", ":m-2<CR>==", noremap)
-vim.keymap.set('i', "<C-Down>", "<Esc>:m+<CR>==gi", noremap)
-vim.keymap.set('i', "<C-Up>", "<Esc>:m-2<CR>==gi", noremap)
-vim.keymap.set('v', "<C-Down>", ":m'>+<CR>gv=gv", noremap)
-vim.keymap.set('v', "<C-Up>", ":m-2<CR>gv=gv", noremap)
-vim.keymap.set('n', "<C-u>", "<C-u>zz", map)
-vim.keymap.set('n', "<C-d>", "<C-d>zz", map)
+-- ______ ______
+--||Ctrl|||←   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-Left>", ":bprev <CR>", {silent = true, noremap})
+-- ______ ______
+--||Ctrl|||→   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-Right>", ":bnext <CR>", {silent = true, noremap})
+-- ______ ______
+--||Ctrl|||↓   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-Down>", ":m+<CR>==", {silent = true, noremap})
+vim.keymap.set('i', "<C-Down>", "<Esc>:m+<CR>==gi", {silent = true, noremap})
+vim.keymap.set('v', "<C-Down>", ":m'>+<CR>gv=gv", {silent = true, noremap})
+-- ______ ______
+--||Ctrl|||↑   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-Up>", ":m-2<CR>==", {silent = true, noremap})
+vim.keymap.set('i', "<C-Up>", "<Esc>:m-2<CR>==gi", {silent = true, noremap})
+vim.keymap.set('v', "<C-Up>", ":m-2<CR>gv=gv", {silent = true, noremap})
+-- ______ ______
+--||Ctrl|||u   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-u>", "<C-u>zz", {silent = true, map})
+-- ______ ______
+--||Ctrl|||d   ||
+--||____|||____||
+--|/____\|/____\|
+vim.keymap.set('n', "<C-d>", "<C-d>zz", {silent = true, map})
