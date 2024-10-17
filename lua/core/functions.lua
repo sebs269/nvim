97edbe39dd337d6a15fd9eb638a4ad320ec06183
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-17 09:25:20 +0200

    refactor: see details

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -1,19 +1,18 @@
 function TrimWhiteSpace()
-    local save = vim.fn.winsaveview()
-    vim.cmd('keeppatterns %s/\\s\\+$//e')
-    vim.fn.winrestview(save)
+  local save = vim.fn.winsaveview()
+  vim.cmd('keeppatterns %s/\\s\\+$//e')
+  vim.fn.winrestview(save)
 end
 
 vim.cmd("command! TrimWhiteSpace lua TrimWhiteSpace()")
 
--- FIXME
 function SearchAllBuffers(str)
-    vim.cmd('cclose')
-    vim.cmd('%argd')
-    vim.cmd('bufdo :arga')
-    vim.cmd('argded')
-    vim.cmd('vimgrep /' .. str .. '/ ##')
-    vim.cmd('copen')
+  vim.cmd('cclose')
+  vim.cmd('%argd')
+  vim.cmd('bufdo :arga')
+  vim.cmd('argded')
+  vim.cmd('vimgrep /' .. str .. '/ ##')
+  vim.cmd('copen')
 end
 
 vim.cmd("command! -nargs=1 SearchAllBuffers lua SearchAllBuffers(<f-args>)")
