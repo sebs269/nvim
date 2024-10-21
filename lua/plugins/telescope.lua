837515eb5345a8f574c29c5ce785826ade69e95b
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-21 12:03:16 +0200

    fix: Telescope lazy loaded on related keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,24 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  event = "VeryLazy",
+  keys = {
+    { "<leader>ff", ":Telescope find_files<CR>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live Grep" },
+    { "<leader>fb", ":Telescope buffers<CR>", desc = "Buffers" },
+    { "<leader>fh", ":Telescope help_tags<CR>", desc = "Help Tags" },
+    -- To be tried & tested
+    { "<leader>fo", ":Telescope oldfiles<CR>", desc = "Old files" },
+    { "<leader>fi", "<cmd>AdvancedGitSearch<CR>", desc = "Git search" },
+    { "<leader>fs", ":Telescope lsp_document_symbols<CR>", desc = "LSP doc symbol" },
+    { "<leader>fm", ":Telescope man_pages<CR>", desc = "Man pages" },
+    { "<leader>f/", ":Telescope current_buffer_fuzzy_find<CR>", desc = "Current buffer fuzzy find" },
+    { "<leader>fc", ":Telescope command_history<CR>", desc = "Command history" },
+  },
   dependencies = {
     {'nvim-lua/plenary.nvim'},
     {'nvim-telescope/telescope-live-grep-args.nvim'},
     {'aaronhallaert/advanced-git-search.nvim'},
   },
-  config = function()
-    local builtin = require('telescope.builtin')
-
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-    -- Deprecated
-    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    --vim.keymap.set('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>')
-
-    -- To be tried & tested
-    vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
-    vim.keymap.set('n', '<leader>fi', '<cmd>AdvancedGitSearch<CR>')
-    vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
-    vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
-    --vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find, {})
-    --vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
-    --vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
-
+  opts = function()
     -- Dependencies
     require("telescope").load_extension("advanced_git_search")
     require("telescope").load_extension("live_grep_args")
