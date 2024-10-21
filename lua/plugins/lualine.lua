56ee3ca3a55a428a91c22be35d68e5ec48e7a373
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-21 11:00:18 +0200

    fix: remove diagnosis from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,8 +5,16 @@ return {
     require('lualine').setup{
     options = {
       icons_enabled = true,
+      --fmt = string.lower,
       --component_separators = '',
       --section_separators = '',
+    },
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch'},
+      lualine_x = {'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
     },
   }
   end
