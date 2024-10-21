66b63fa0fd8a3851b3f28ca506e2e20171ab5714
Sebastien SAUZON <sebastien.sauzon-ext@st.com>
2024-10-21 16:53:22 +0200

    feat: make keymaps silents when lazy loading Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,17 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   keys = {
-    { "<leader>ff", ":Telescope find_files<CR>", desc = "Find Files" },
-    { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live Grep" },
-    { "<leader>fb", ":Telescope buffers<CR>", desc = "Buffers" },
-    { "<leader>fh", ":Telescope help_tags<CR>", desc = "Help Tags" },
+    { "<leader>ff", ":Telescope find_files<CR>", desc = "Find Files", silent = true },
+    { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live Grep", silent = true },
+    { "<leader>fb", ":Telescope buffers<CR>", desc = "Buffers", silent = true },
+    { "<leader>fh", ":Telescope help_tags<CR>", desc = "Help Tags", silent = true },
     -- To be tried & tested
-    { "<leader>fo", ":Telescope oldfiles<CR>", desc = "Old files" },
-    { "<leader>fi", "<cmd>AdvancedGitSearch<CR>", desc = "Git search" },
-    { "<leader>fs", ":Telescope lsp_document_symbols<CR>", desc = "LSP doc symbol" },
-    { "<leader>fm", ":Telescope man_pages<CR>", desc = "Man pages" },
-    { "<leader>f/", ":Telescope current_buffer_fuzzy_find<CR>", desc = "Current buffer fuzzy find" },
-    { "<leader>fc", ":Telescope command_history<CR>", desc = "Command history" },
+    { "<leader>fo", ":Telescope oldfiles<CR>", desc = "Old files", silent = true },
+    { "<leader>fi", "<cmd>AdvancedGitSearch<CR>", desc = "Git search", silent = true },
+    { "<leader>fs", ":Telescope lsp_document_symbols<CR>", desc = "LSP doc symbol", silent = true },
+    { "<leader>fm", ":Telescope man_pages<CR>", desc = "Man pages", silent = true },
+    { "<leader>f/", ":Telescope current_buffer_fuzzy_find<CR>", desc = "Current buffer fuzzy find", silent = true },
+    { "<leader>fc", ":Telescope command_history<CR>", desc = "Command history", silent = true },
   },
   dependencies = {
     {'nvim-lua/plenary.nvim'},
